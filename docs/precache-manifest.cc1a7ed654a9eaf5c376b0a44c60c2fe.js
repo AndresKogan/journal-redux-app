19562b68ce40fe001c0eb696998bccde5093e025
AndresKogan <61512404+AndresKogan@users.noreply.github.com>
2021-11-23 19:22:11 -0300

    Docs Added

---
+++ docs/precache-manifest.cc1a7ed654a9eaf5c376b0a44c60c2fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01768dd96ad4c8ee54779c21f17eec2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43985e1c74bb141b003e",
+    "url": "/static/css/main.2630c656.chunk.css"
+  },
+  {
+    "revision": "5d1259851a4ed804f2cb",
+    "url": "/static/js/2.113f89b1.chunk.js"
+  },
+  {
+    "revision": "75fc5a0ff22eb52fcd50272201589016",
+    "url": "/static/js/2.113f89b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43985e1c74bb141b003e",
+    "url": "/static/js/main.20c5ee63.chunk.js"
+  },
+  {
+    "revision": "1f9b80c10cb2174ac7b3",
+    "url": "/static/js/runtime-main.8c0f0de7.js"
+  }
+]);

ebb66c1611a8248c730e696bb5130278e5d0d7c4
AndresKogan <61512404+AndresKogan@users.noreply.github.com>
2021-11-23 22:09:11 -0300

    Journal App

---
+++ docs/precache-manifest.ff8bf89f5935d10d9c3e37387d9e1a20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9877441d0ef865501e5f51fb07c2ddf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51dc6928f2e6ebc218cc",
+    "url": "/static/css/main.2630c656.chunk.css"
+  },
+  {
+    "revision": "f0c2dcd9d3995f7fae9b",
+    "url": "/static/js/2.f2d095e4.chunk.js"
+  },
+  {
+    "revision": "75fc5a0ff22eb52fcd50272201589016",
+    "url": "/static/js/2.f2d095e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51dc6928f2e6ebc218cc",
+    "url": "/static/js/main.bf769838.chunk.js"
+  },
+  {
+    "revision": "1f9b80c10cb2174ac7b3",
+    "url": "/static/js/runtime-main.8c0f0de7.js"
+  }
+]);
